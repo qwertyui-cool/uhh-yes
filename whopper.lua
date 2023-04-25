b2c8d4c1d07fa92de357b527f1aba760b8a425a1
qwertyui-cool <112367698+qwertyui-cool@users.noreply.github.com>
2023-04-24 23:15:06 -0500

    v1.0

--- whopper.lua
+++ whopper.lua
@@ -1,16 +1,17 @@
 local id = game.PlaceId
-local game = ""
+local place = ""
 local function loadScript(f)
-	local path = f
-	loadstring(game:HttpGet("https://raw.githubusercontent/qwertyui-cool/uhh-yes/main/"..path))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/qwertyui-cool/uhh-yes/main/"..f))()
 end
 local function check(i)
-	if i == 6403373529 then game = "slap%20battles" return end
-	if i == 9015014224 then game = "slap%20battles" return end
+	if i == 6403373529 then place = "slap%20battles" return end
+	if i == 9015014224 then place = "slap%20battles" return end
+	if i == 11520107397 then place = "slap%20battles" return end
 	error("unsupported game")
 end
 local function load(g)
 	loadScript("games/"..g..".lua")
+	print("loaded")
 end
 check(id)
-load(game)
+load(place)
