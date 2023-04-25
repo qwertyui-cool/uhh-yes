ff8e5b5fa38bcecbdea1a3493c2f1a8939b2e1da
qwertyui-cool <112367698+qwertyui-cool@users.noreply.github.com>
2023-04-24 20:26:10 -0500

    Update i think this is the loadstring or smth.lua

--- i think this is the loadstring or smth.lua
+++ i think this is the loadstring or smth.lua
@@ -5,8 +5,8 @@ local function loadScript(f)
 	loadstring(game:HttpGet("https://raw.githubusercontent/qwertyui-cool/uhh-yes/main/"..path))()
 end
 local function check(i)
-	if i == 6403373529 then game = "slap battles" return end
-	if i == 9015014224 then game = "slap battles" return end
+	if i == 6403373529 then game = "slap%20battles" return end
+	if i == 9015014224 then game = "slap%20battles" return end
 	error("unsupported game")
 end
 local function load(g)
